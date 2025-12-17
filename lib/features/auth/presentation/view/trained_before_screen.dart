db438a44ffd0990e510230dc524443605bbe41d6
Yaseen Usmani <yaseenusmani654@gmail.com>
2025-12-17 14:26:08 +0500

    Update bottom sheet Ui

--- lib/features/auth/presentation/view/trained_before_screen.dart
+++ lib/features/auth/presentation/view/trained_before_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:startup_repo/core/widgets/sign_in_button.dart';
+import 'package:startup_repo/core/widgets/getstarted_button.dart';
 import 'package:startup_repo/imports.dart';
 import '../controller/auth_controller.dart';
 
@@ -14,6 +15,9 @@ class _TrainedBeforeScreenState extends State<TrainedBeforeScreen> {
   final controller = Get.find<AuthController>();
   int selectedLevel = 1;
   int selectedDay = 1;
+  final List<BaselineMeasurement> _baselineMeasurements = [];
+  final TextEditingController _valueController = TextEditingController();
+  DateTime? _selectedDate;
 
   final List<String> levelList = [
     'Level 1',
@@ -39,6 +43,12 @@ class _TrainedBeforeScreenState extends State<TrainedBeforeScreen> {
     'Day 11',
     'Day 12'
   ];
+
+  @override
+  void dispose() {
+    _valueController.dispose();
+    super.dispose();
+  }
 
   void _onConfirm() {
     controller.setTrainedBeforeDetails(
@@ -303,7 +313,7 @@ class _TrainedBeforeScreenState extends State<TrainedBeforeScreen> {
                           ),
                           SizedBox(width: 10.w),
                           GestureDetector(
-                            onTap: () {},
+                            onTap: () => _showManualInputDialog(context),
                             child: Container(
                               width: 24.w,
                               height: 24.w,
@@ -338,17 +348,24 @@ class _TrainedBeforeScreenState extends State<TrainedBeforeScreen> {
                           height: 1.2,
                         ),
                       ),
-                      SizedBox(height: 42.h),
-                      Center(
-                        child: Text(
-                          'None specified',
-                          style: TextStyle(
-                            color: Color(0xFF777576),
-                            fontSize: 17.sp,
-                            fontWeight: FontWeight.w500,
+                      SizedBox(height: 24.h),
+                      if (_baselineMeasurements.isEmpty)
+                        Center(
+                          child: Text(
+                            'None specified',
+                            style: TextStyle(
+                              color: Color(0xFF777576),
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                        ),
-                      ),
+                        )
+                      else
+                        ..._baselineMeasurements.map((measurement) => Padding(
+                              padding: EdgeInsets.only(bottom: 12.h),
+                              child: _buildMeasurementCard(measurement),
+                            )),
+                      SizedBox(height: 20.h),
                     ],
                   ),
                 ),
@@ -412,4 +429,281 @@ class _TrainedBeforeScreenState extends State<TrainedBeforeScreen> {
       ],
     );
   }
+
+  void _showManualInputDialog(BuildContext context) {
+    _valueController.clear();
+    _selectedDate = DateTime.now();
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (context) => StatefulBuilder(
+        builder: (context, setDialogState) => Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF191919),
+                Color(0xFF252525),
+              ],
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.r),
+              topRight: Radius.circular(30.r),
+            ),
+          ),
+          padding: EdgeInsets.only(
+            left: 25.w,
+            right: 25.w,
+            top: 30.h,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 30.h,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Text(
+                    'Manual Input',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Container(
+                        width: 32.w,
+                        height: 32.w,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF4CAF50),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 20.sp,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 80.w,
+                    child: TextField(
+                      controller: _valueController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.left,
+                      cursorColor: const Color(0xFF4CAF50),
+                      onChanged: (value) => setDialogState(() {}),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 48.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: '0',
+                        hintStyle: TextStyle(
+                          color: Colors.grey[500],
+                          fontSize: 60.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        filled: false,
+                        border: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.zero,
+                        isDense: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30.w),
+                  Text(
+                    'MPH',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28.sp,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 28.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Measured',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      final date = await showDatePicker(
+                        context: context,
+                        initialDate: _selectedDate ?? DateTime.now(),
+                        firstDate: DateTime(2020),
+                        lastDate: DateTime.now(),
+                        builder: (context, child) {
+                          return Theme(
+                            data: ThemeData.dark(),
+                            child: child!,
+                          );
+                        },
+                      );
+                      if (date != null) {
+                        setDialogState(() {
+                          _selectedDate = date;
+                        });
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 6.h),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[600],
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                      child: Text(
+                        _selectedDate != null
+                            ? '${_selectedDate!.day} ${_getMonthName(_selectedDate!.month)} ${_selectedDate!.year}'
+                            : 'Select Date',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.sp,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25.h),
+              Builder(
+                builder: (context) {
+                  final isValid = _valueController.text.isNotEmpty && _selectedDate != null;
+                  return AbsorbPointer(
+                    absorbing: !isValid,
+                    child: Opacity(
+                      opacity: isValid ? 1.0 : 0.5,
+                      child: GradientButton(
+                        onPressed: () {
+                          if (_valueController.text.isNotEmpty && _selectedDate != null) {
+                            setState(() {
+                              _baselineMeasurements.add(
+                                BaselineMeasurement(
+                                  value: _valueController.text,
+                                  date: _selectedDate!,
+                                ),
+                              );
+                            });
+                            Navigator.pop(context);
+                          }
+                        },
+                        text: 'Add',
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.w800,
+                        height: 64.h,
+                        borderRadius: 12.0,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMeasurementCard(BaselineMeasurement measurement) {
+    return Container(
+      padding: EdgeInsets.all(16.w),
+      decoration: BoxDecoration(
+        color: Color(0xFF1A1A1A),
+        borderRadius: BorderRadius.circular(12.r),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${measurement.value} MPH',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 4.h),
+                Text(
+                  '${_getMonthName(measurement.date.month)} ${measurement.date.day}, ${measurement.date.year} • Baseline Test',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                    fontSize: 14.sp,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _baselineMeasurements.remove(measurement);
+              });
+            },
+            child: Icon(
+              Icons.delete_sharp,
+              color: const Color.fromARGB(255, 219, 78, 78),
+              size: 24.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getMonthName(int month) {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    return months[month - 1];
+  }
+}
+
+class BaselineMeasurement {
+  final String value;
+  final DateTime date;
+
+  BaselineMeasurement({
+    required this.value,
+    required this.date,
+  });
 }
